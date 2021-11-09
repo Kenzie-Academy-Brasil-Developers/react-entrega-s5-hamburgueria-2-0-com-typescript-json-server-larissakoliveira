7aaba87041e95bfad1ca1dd56bf52482dcf3c872
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-11-08 21:30:56 -0300

    feat: buttons added

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,18 @@
-import { Box, Grid, Image } from "@chakra-ui/react";
+import { Box, Button, Grid, Image } from "@chakra-ui/react";
 import { useProducts } from "../../providers/Products";
 
 const Card = () => {
   const { products } = useProducts();
 
   return (
-    <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+    <Grid padding='25px' templateColumns="repeat(4, 1fr)" gap={280}>
       {products.map((item) => (
-        <Box w="100%" h="10">
-        <Image src={item.image} alt={item.title}/>
+        <Box w="100%" h="7">
+        <Image height='158px' widht='158px' src={item.image} alt={item.title}/>
         <h2>{item.title}</h2>
         <p>{item.category}</p>
-        <h3>{item.price}</h3>
+        <h3>{item.price.toFixed(2)}</h3>
+        <Button >Adicionar</Button>
         </Box>
       ))}
     </Grid>
