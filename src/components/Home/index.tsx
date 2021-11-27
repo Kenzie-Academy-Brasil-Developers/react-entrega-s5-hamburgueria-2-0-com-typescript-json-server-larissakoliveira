4845398e99592d8d06938096bf09e13f564d7f94
Larissa K Oliveira <larissakoliveira@pop-os.localdomain>
2021-11-27 10:28:59 -0300

    adding garfild pic

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -26,6 +26,7 @@ import { theme } from "../../styles/theme";
 import { useProducts } from "../../providers/Products";
 import { useState } from "react";
 import { Navigate } from "react-router";
+import profilePic from "../../assets/imgs/garfieldProfile.png"
 
 const Home = () => {
 
@@ -53,53 +54,65 @@ const Home = () => {
         padding="10px"
         position="fixed"
         flexDirection="row"
+        justifyContent="space-around"
       >
-        <Image  ml='50px'width="195px" height="45px" src={logo} />
-        <InputGroup flexDirection="column">
+        <Image width="195px" height="45px" src={logo} />
+        <Flex alignItems= 'center' >
+
+        <InputGroup mr='15px' flexDirection="column">
           <Input
             name="Pesquisa"
             placeholder="Digitar pesquisa"
             width="300px"
-            height="45px"
-            left="800px"
+            height="40px" 
             border-radius="8px"
             padding="[0px, 10px, 0px, 15px]"
             bg="white"
             onChange={(event) => setInputValue(event.target.value)}
-          />
+            />
           <InputRightElement
             onClick={() => productNameFiltered(inputValue)}
             cursor="pointer"
             borderRadius="8px"
+            height="40px" 
+            marginRight='8px'
             bg={theme.colors.green.primary50}
-          >
-              <FaSearch />
+            >
+              <FaSearch color='white' />
           </InputRightElement>
         </InputGroup>
 
         <ModalComponent />
-        <Button marginRight='70px' padding="0" colorScheme="white" onClick={onOpen}>
+        <Button ml='50px' padding="2px" colorScheme="white" onClick={onOpen}>
           <HiOutlineLogout size="2x" color={theme.colors.green.primary50} />
         </Button>
+            </Flex>
         <Drawer placement="top" onClose={onClose} isOpen={isOpen}>
-          <DrawerOverlay mt={["13vh", "8vh"]} />
-          <DrawerContent ml="auto" mt="55px" w={["450px", "350px"]}>
+          <DrawerOverlay mt={["13vh", "10vh"]} />
+          <DrawerContent backgroundColor='#ecf0f1' borderRadius='5px' ml="auto" mt="58px" w={["450px", "350px"]}>
             <DrawerHeader
+            padding='15px 5px 5px 5px'
               borderBottomWidth="1px"
               borderColor="gray.50"
               color="gray.400"
+              display='flex'
+              justifyContent='space-around'
+              alignItems='center'
             >
+              <Text>
               {user.email}
+              </Text>
+              <Image borderRadius='8px' w='85px' h='80px' src={profilePic} />
             </DrawerHeader>
-            <DrawerBody paddingTop='0'>
+            <DrawerBody marginBottom='5px'paddingTop='15px'>
               <Flex
                 align="center"
                 onClick={logout}
                 _hover={{ cursor: "pointer" }}
               >
                 <Center
-                  w="60px"
-                  h="60px"
+                  w="45px"
+                  h="45px"
                   bg="red.600"
                   fontSize="2xl"
                   borderRadius="md"
@@ -113,7 +126,7 @@ const Home = () => {
                   <Heading as="h2" fontSize="lg">
                     Já vai {user.name}?
                   </Heading>
-                  <Text color="gray.600" fontSize="small">
+                  <Text color="gray.600" fontSize="sm">
                     Sim, partiu academia agora!
                   </Text>
                 </Box>
